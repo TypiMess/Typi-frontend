754781be8340321fd0ab4b5b9df8d75449fef83a
Duc Nguyen <tom@ducng.dev>
2021-07-16 00:29:08 +1200

    Fixed Alert

--- src/views/Alert.tsx
+++ src/views/Alert.tsx
@@ -1,11 +1,20 @@
-import React from "react";
+import React, { MouseEventHandler } from "react";
 
-export default class Alert extends React.Component {
+interface IProps {
+    onClose: MouseEventHandler
+}
+
+export default class Alert extends React.Component<IProps> {
+    constructor(props: IProps)
+    {
+        super(props);
+    }
+    
     render() {
         return (
             <div className="alert alert-danger alert-dismissible fade show" role="alert">
                 { this.props.children }
-                <button type="button" className="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                <button type="button" className="btn-close" data-bs-dismiss="alert" aria-label="Close" onClick={this.props.onClose}></button>
             </div>
         )
     }
--- src/views/homepage-components/Login.tsx
+++ src/views/homepage-components/Login.tsx
@@ -1,4 +1,4 @@
-import React, { ChangeEvent, FormEvent, ReactElement } from "react";
+import React, { ChangeEvent, FormEvent } from "react";
 import CredentialsController from "../../controllers/CredentialsController";
 import CurrentUserController from "../../controllers/CurrentUserController";
 import InvalidError from "../../errors/InvalidError";
@@ -7,7 +7,7 @@ import UnauthorizedError from "../../errors/UnauthorizedError";
 import Alert from "../Alert";
 
 interface IStates {
-    alert: ReactElement,
+    alertText: string,
     username: string,
     password: string
 }
@@ -15,19 +15,23 @@ interface IStates {
 export default class Login extends React.Component<{}, IStates> {
     constructor(props: {}) {
         super(props);
-        
+
         this.state = {
-            alert: <></>,
+            alertText: "",
             username: "",
             password: ""
         }
-        
+
+        this.onCloseAlert = this.onCloseAlert.bind(this);
         this.handleLoginForm = this.handleLoginForm.bind(this);
         this.handleInputChange = this.handleInputChange.bind(this);
     }
-    
-    handleInputChange(event: ChangeEvent<HTMLInputElement>)
-    {
+
+    onCloseAlert() {
+        this.setState({ alertText: "" });
+    }
+
+    handleInputChange(event: ChangeEvent<HTMLInputElement>) {
         const target = event.target;
         const value = target.type === 'checkbox' ? target.checked : target.value;
         const name = target.name;
@@ -35,44 +39,39 @@ export default class Login extends React.Component<{}, IStates> {
             [name]: value
         } as Pick<IStates, any>);
     }
-    
+
     handleLoginForm(event: FormEvent) {
         event.preventDefault();
-        
+
         CredentialsController.Instance.Login(this.state.username, this.state.password)
-        .then(result => {
-            CurrentUserController.Update().catch(error => {
-                if (error instanceof UnauthorizedError)
-                {
-                    this.setState({ alert: <Alert>Invalid session. Please enable cookies for this site.</Alert> });
+            .then(result => {
+                CurrentUserController.Update().catch(error => {
+                    if (error instanceof UnauthorizedError) {
+                        this.setState({ alertText: "Invalid session. Please enable cookies for this site." });
+                    }
+                    else {
+                        this.setState({ alertText: "An unknown error has occured. Sorry about this! We will work harder to fix this issue. If you face this error again, please report it to us." });
+                    }
+                });
+            })
+            .catch(error => {
+                if (error instanceof NotFoundError) {
+                    this.setState({ alertText: "The username and password combination does not match. Please check again." });
                 }
-                else
-                {
-                    this.setState({ alert: <Alert>An unknown error has occured. Sorry about this! We will work harder to fix this issue. If you face this error again, please report it to us.</Alert> });
+                else if (error instanceof InvalidError) {
+                    this.setState({ alertText: "The username or password is invalid. Please check again." });
                 }
-            });
-        })
-        .catch(error => {            
-            if (error instanceof NotFoundError)
-            {
-                this.setState({ alert: <Alert>The username and password combination does not match. Please check again.</Alert> });
-            }
-            else if (error instanceof InvalidError)
-            {
-                this.setState({ alert: <Alert>The username or password is invalid. Please check again.</Alert> });
-            }
-            else
-            {
-                this.setState({ alert: <Alert>An unknown error has occured. Sorry about this! We will work harder to fix this issue. If you face this error again, please report it to us.</Alert> });
-            }
-        })
+                else {
+                    this.setState({ alertText: "An unknown error has occured. Sorry about this! We will work harder to fix this issue. If you face this error again, please report it to us." });
+                }
+            })
     }
-    
+
     render() {
         return (
             <div className="container rounded shadow p-3 bg-white">
                 <h2 className="text-center">Login</h2>
-                { this.state.alert }
+                {this.state.alertText ? <Alert onClose={this.onCloseAlert}>{this.state.alertText}</Alert> : <></>}
                 <form onSubmit={this.handleLoginForm}>
                     <div className="mb-3">
                         <label className="form-label" htmlFor="username">Username:</label>
@@ -80,7 +79,7 @@ export default class Login extends React.Component<{}, IStates> {
                     </div>
                     <div className="mb-3">
                         <label className="form-label" htmlFor="password">Password:</label>
-                        <input className="form-control" type="password" minLength={7} name="password" id="password" onChange={this.handleInputChange}/>
+                        <input className="form-control" type="password" minLength={7} name="password" id="password" onChange={this.handleInputChange} />
                     </div>
                     <button className="btn btn-primary" type="submit">Login</button>
                 </form>
