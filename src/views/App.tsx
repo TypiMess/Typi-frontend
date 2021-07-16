e82c161353c734d59d6ad05ef4385742097086da
Duc Nguyen <tom@ducng.dev>
2021-07-16 21:36:53 +1200

    Hide App before finished loading - Loading is done after verifying session and get user if possible

--- src/views/App.tsx
+++ src/views/App.tsx
@@ -5,44 +5,57 @@ import SessionsController from '../controllers/SessionsController';
 import CurrentUserController from '../controllers/CurrentUserController';
 
 interface IStates {
-    isLoggedIn: boolean
+    isLoggedIn: boolean,
+    isLoading: boolean
 }
 
 class App extends React.Component<{}, IStates> {
-    constructor(props: {})
-    {
+    constructor(props: {}) {
         super(props);
-        
+
         this.state = {
-            isLoggedIn: false
+            isLoggedIn: false,
+            isLoading: true
         }
-        
+
         this.handleOnLogout = this.handleOnLogout.bind(this);
     }
-    
+
     componentDidMount() {
-        CurrentUserController.AddOnReadyListener(this, (component: React.Component) => component.setState({ isLoggedIn: true }));
+        CurrentUserController.AddOnReadyListener(this, (component: React.Component) => {
+            component.setState({ isLoggedIn: true });
+        });
+
         SessionsController.VerifySession().then(valid => {
             if (valid) {
-                CurrentUserController.Update().catch(error => console.error(error));
+                CurrentUserController.Update()
+                    .catch(error => console.error(error))
+                    .finally(() => this.setState({ isLoading: false }));
+            }
+            else {
+                this.setState({ isLoading: false });
             }
         });
     }
-    
-    handleOnLogout()
-    {
+
+    handleOnLogout() {
         this.setState({ isLoggedIn: false });
     }
 
     render() {
         return (
-            <div className="App">
+            <>
                 {
-                    this.state.isLoggedIn ?
-                        <ChatApp onLogout={this.handleOnLogout}/> :
-                        <Homepage />
+                    !this.state.isLoading &&
+                    <div className="App">
+                        {
+                            this.state.isLoggedIn ?
+                                <ChatApp onLogout={this.handleOnLogout} /> :
+                                <Homepage />
+                        }
+                    </div>
                 }
-            </div>
+            </>
         )
     }
 }
