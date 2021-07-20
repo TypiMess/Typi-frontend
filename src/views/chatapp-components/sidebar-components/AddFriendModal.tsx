a6dd71bba3588b77df536a6ef525255c0451baf5
Duc Nguyen <tom@ducng.dev>
2021-07-20 23:00:48 +1200

    Added form to AddFriendModal: support Enter press

--- src/views/chatapp-components/sidebar-components/AddFriendModal.tsx
+++ src/views/chatapp-components/sidebar-components/AddFriendModal.tsx
@@ -18,7 +18,7 @@ export default class AddFriendModal extends React.Component<{}, IStates> {
         }
 
         this.handleTargetUsernameChange = this.handleTargetUsernameChange.bind(this);
-        this.handleSendRequestClick = this.handleSendRequestClick.bind(this);
+        this.handleSendRequest = this.handleSendRequest.bind(this);
     }
 
     handleTargetUsernameChange(event: ChangeEvent<HTMLInputElement>) {
@@ -27,7 +27,9 @@ export default class AddFriendModal extends React.Component<{}, IStates> {
         this.setState({ targetUsername: value });
     }
 
-    handleSendRequestClick() {
+    handleSendRequest(event: React.FormEvent|React.MouseEvent) {
+        event.preventDefault();
+        
         RelationshipsController.Instance.SendFriendRequest(this.state.targetUsername)
             .then(() => {
                 this.setState({ targetUsername: "" });
@@ -43,7 +45,7 @@ export default class AddFriendModal extends React.Component<{}, IStates> {
                     NotificationContainer.AddNotification({ title: "Not found", type: "warning", body: <>The username you entered does not match any user. Please check again.</> });
                 }
                 else if (error instanceof DuplicateError) {
-                    NotificationContainer.AddNotification({ title: "More than friends?", type: "warning", body: <>You are already friends with <strong>{this.state.targetUsername}</strong></> });
+                    NotificationContainer.AddNotification({ title: "More than friends?", type: "normal", body: <>You are already friends with <strong>{this.state.targetUsername}</strong></> });
                 }
                 else {
                     NotificationContainer.AddNotification({ title: "Error", type: "error", body: <>An unknown error has occured. Sorry for the inconvenience.</> });
@@ -54,10 +56,12 @@ export default class AddFriendModal extends React.Component<{}, IStates> {
     render() {
         return (
             <Modal title="Add friend" additionalFooter={
-                <button className="btn btn-primary" onClick={this.handleSendRequestClick}>Send request</button>
+                <button className="btn btn-primary" onClick={this.handleSendRequest}>Send request</button>
             }>
+            <form onSubmit={this.handleSendRequest}>
                 <label className="form-label" htmlFor="targetUsername">Their username:</label>
                 <input className="form-control" pattern="(\w|\d){3,32}" id="targetUsername" name="targetUsername" value={this.state.targetUsername} onChange={this.handleTargetUsernameChange} />
+            </form>
             </Modal>
         )
     }
